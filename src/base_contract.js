89af478a0365d09e7bb023af0a20c71ca339a65f
Diep Pham <diep@kyber.network>
2018-08-20 14:44:52 +0700

    implement BaseContract

--- src/base_contract.js
+++ src/base_contract.js
@@ -1,6 +1,12 @@
 import Web3 from 'web3'
 import baseContractABI from './base_contract_abi'
 
+const validateAddress = address => {
+  if (!Web3.utils.isAddress(address)) {
+    throw new Error(`invalid address: '${address}'`)
+  }
+}
+
 /**
  * BaseContract contains common methods for all contracts of a KyberNetwork
  * reserve.
@@ -15,9 +21,8 @@ export default class BaseContract {
     if (!provider) {
       throw new Error('missing provider')
     }
-    if (!Web3.utils.isAddress(address)) {
-      throw new Error(`invalid address: '${address}'xx`)
-    }
+
+    validateAddress(address)
 
     const web3 = new Web3(provider)
     this.contract = new web3.eth.Contract(baseContractABI, address)
@@ -27,7 +32,7 @@ export default class BaseContract {
    * Return the current admin address of contract.
    * @return {string} - current admin address
    */
-  async admin () {
+  admin () {
     return this.contract.methods.admin().call()
   }
 
@@ -37,55 +42,80 @@ export default class BaseContract {
    * claimed yet.
    * @return {string} - pending admin address
    */
-  async pendingAdmin () {
+  pendingAdmin () {
     return this.contract.methods.pendingAdmin().call()
+  }
+
+  /**
+   * transfer admin privilege to given address.
+   * @param {object} account - current admin account
+   * @param {string} address - new admin address
+   */
+  transferAdmin (account, address) {
+    validateAddress(address)
+    return this.contract.methods.transferAdmin(address).send({ from: account })
+  }
+
+  /**
+   * Claim admin privilege. The account address should be in already placed
+   * in pendingAdmin for this to works.
+   */
+  claimAdmin (account) {
+    return this.contract.methods.claimAdmin().send({ from: account })
   }
 
   /**
    * Return operator addresses of contract.
    * @return {array} - list of operator addresses
    */
-  getOperators () {}
-
-  /**
-   * Return alerter addresses of contract.
-   * @return {array} - list of alerter addresses
-   */
-  getAlerters () {}
-
-  /**
-   * transfer admin privilege to given address.
-   * @param {string} address - new admin address
-   */
-  transferAdmin (address) {}
-
-  /**
-   * Claim admin privilege. The account address should be in already placed
-   * in pendingAdmin for this to works.
-   */
-  claimAdmin () {}
+  getOperators () {
+    return this.contract.methods.getOperators().call()
+  }
 
   /**
    * Add given address from operators list.
+   * @param {object} account - current admin account
    * @param {string} address - address to remove from operators list.
    */
-  addOperator (address) {}
+  addOperator (account, address) {
+    validateAddress(address)
+    return this.contract.methods.addOperator(address).send({ from: account })
+  }
 
   /**
    * Remove given address from operators list.
    * @param {string} address - address to remove from operators list.
    */
-  removeOperator (address) {}
+  removeOperator (address) {
+    validateAddress(address)
+    return this.contract.methods.removeOperator(address).send({ from: account })
+  }
+
+  /**
+   * Return alerter addresses of contract.
+   * @return {array} - list of alerter addresses
+   */
+  getAlerters () {
+    return this.contract.methods.getAlerters().call()
+  }
 
   /**
    * Add new address to alerters list.
+   * @param {object} account - current admin account
    * @param {string} address - address to add to alerters list.
    */
-  addAlerter (address) {}
+  addAlerter (account, address) {
+    validateAddress(address)
+    return this.contract.methods.addAlerter(address).send({ from: account })
+  }
 
   /**
    * Remove address from alerters list.
+   * @param {object} account - current admin account
    * @param {string} address - address to remove from alerters list.
    */
-  removeAlerter (address) {}
+  removeAlerter (address) {
+    validateAddress(address)
+    return this.contract.methods.removeAlerter(address).send({ from: account })
+  }
 }
--- test/base_contract.test.js
+++ test/base_contract.test.js
@@ -6,6 +6,7 @@ import BaseContract from '../src/base_contract'
 const infuraAPI = process.env.INFURA_API
 // TODO: use ganache provider instead once Deployer is implemented
 const provider = new Web3.providers.HttpProvider(infuraAPI)
+const web3 = new Web3(provider)
 const devBaseContractAddress = '0x798AbDA6Cc246D0EDbA912092A2a3dBd3d11191B'
 
 describe('BaseContract', () => {
@@ -25,19 +26,94 @@ describe('BaseContract', () => {
   })
 
   it('created an instance successfully', () => {
-    let baseContract = new BaseContract(provider, devBaseContractAddress)
+    const baseContract = new BaseContract(provider, devBaseContractAddress)
     assert.ok(baseContract.contract)
   })
 
-  it('could view admin address', done => {
+  it('could view admin address', async () => {
     const baseContract = new BaseContract(provider, devBaseContractAddress)
-    ;(async () => {
-      const admin = await baseContract.admin()
-      assert.ok(Web3.utils.isAddress(admin))
+    const admin = await baseContract.admin()
+    assert.ok(Web3.utils.isAddress(admin))
 
-      const pendingAdmin = await baseContract.pendingAdmin()
-      assert.ok(Web3.utils.isAddress(pendingAdmin))
-    })()
-    done()
+    const pendingAdmin = await baseContract.pendingAdmin()
+    assert.ok(Web3.utils.isAddress(pendingAdmin))
+  })
+
+  // TODO: this task always failed because no contract is deployed in
+  // test network
+  it('could transfer admin account', async () => {
+    const accounts = await web3.eth.getAccounts()
+    const baseContract = new BaseContract(provider, devBaseContractAddress)
+    const currentAdmin = accounts[0]
+    const newAdmin = accounts[1]
+    let admin, pendingAdmin
+
+    await baseContract.transferAdmin(currentAdmin, newAdmin.address)
+    admin = await baseContract.admin()
+    assert.equal(admin, currentAdmin.address)
+    pendingAdmin = await baseContract.pendingAdmin()
+    assert.equal(pendingAdmin, newAdmin.address)
+
+    await baseContract.claimAdmin(newAdmin)
+    admin = await baseContract.admin()
+    assert.equal(admin, newAdmin.address)
+    pendingAdmin = await baseContract.pendingAdmin()
+    assert.equal(pendingAdmin, '')
+  })
+
+  it('could get list of all operators', async () => {
+    const baseContract = new BaseContract(provider, devBaseContractAddress)
+    let operators
+
+    operators = await baseContract.getOperators()
+    assert.ok(operators)
+  })
+
+  // TODO: this task always failed because no contract is deployed in
+  // test network
+  it('could change operator address', async () => {
+    const baseContract = new BaseContract(provider, devBaseContractAddress)
+    const accounts = await web3.eth.getAccounts()
+    const currentAdmin = accounts[0]
+    const newOperator = accounts[1]
+    let operators
+
+    operators = await baseContract.getOperators()
+    assert.ok(!operators.include(newOperator))
+
+    await baseContract.addOperator(currentAdmin, newOperator)
+    operators = await baseContract.getOperators()
+    assert.ok(operators.include(newOperator))
+
+    await baseContract.removeOperator(currentAdmin, newOperator)
+    assert.ok(!operators.include(operator))
+  })
+
+  it('could get list of all alerters', async () => {
+    const baseContract = new BaseContract(provider, devBaseContractAddress)
+    let alerters
+
+    alerters = await baseContract.getAlerters()
+    assert.ok(alerters)
+  })
+
+  // TODO: this task always failed because no contract is deployed in
+  // test network
+  it('could change alerter address', async () => {
+    const baseContract = new BaseContract(provider, devBaseContractAddress)
+    const accounts = await web3.eth.getAccounts()
+    const currentAdmin = accounts[0]
+    const newAlerter = accounts[1]
+    let alerters
+
+    alerters = await baseContract.getAlerters()
+    assert.ok(!alerters.include(newAlerter))
+
+    await baseContract.addAlerter(currentAdmin, newAlerter)
+    alerters = await baseContract.getAlerters()
+    assert.ok(alerters.include(newAlerter))
+
+    await baseContract.removeAlerter(currentAdmin, newAlerter)
+    assert.ok(!alerters.include(newAlerter))
   })
 })
