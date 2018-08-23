5718e04734baa7ed12c4e533ce3ded2e9b085d16
kyberliem <cyberliem.civil@gmail.com>
2018-08-23 15:17:39 +0700

    implemented enableTrade and contract addresses methods

---
+++ src/reserve_contract.js
@@ -0,0 +1,62 @@
+import Web3 from 'web3'
+import reserveContractABI from '../contracts/KyberReserveContract.abi'
+import BaseContract from './base_contract'
+
+
+const validateAddress = address => {
+  if (!Web3.utils.isAddress(address)) {
+    throw new Error(`invalid address: '${address}'`)
+  }
+}
+
+/**
+ * ReserveContract contains extended methods for KyberReserveContract
+ */
+export default class ReserveContract extends BaseContract {
+  /**
+   * Create new BaseContract instance.
+   * @param {object} provider - Web3 provider
+   * @param {string} address - address of kyber reserver smart contract.
+   */
+  constructor (provider, address) {
+    super(provider,address)
+    const web3 = new Web3(provider)
+    this.contract = new web3.eth.Contract(reserveContractABI, address)
+  }
+
+  enableTrade (account) {
+    return this.contract.methods.enableTrade().send({ from: account.address })
+  }
+  tradeEnabled () {
+    return this.contract.methods.tradeEnabled().call()
+  }
+  disableTrade (account) {
+    return this.contract.methods.disableTrade().send({ from: account.address })
+  }
+  /**
+   * set Contract addresses for reserver contract.
+   * @param {object} account - admin account.
+   * @param {string} network - address of kyber network smart contract.
+   * @param {string} conversion - address of kyber network smart contract.
+   * @param {string} sanitys (optional) - address of sanity rates contract.
+   * @returns {txObject} - the tx object of send() command from this contract method
+   */
+  setContracts(account, network, conversion, sanity) {
+    validateAddress(network)
+    validateAddress(conversion)
+    return this.contract.methods.setContracts(network, conversion, sanity).send({from : account.address})
+  }
+  
+  conversionRatesContract() {
+      return this.contract.methods.conversionRatesContract().call()
+  }
+
+  sanityRatesContract() {
+      return this.contract.methods.sanityRatesContract().call() 
+  }
+
+  kyberNetwork() {
+      return this.contract.methods.kyberNetwork().call() 
+  }
+
+}
---
+++ test/reserve_contract.test.js
@@ -0,0 +1,83 @@
+import assert from 'assert'
+import Web3 from 'web3'
+import ganache from 'ganache-cli'
+
+import ReserveContract from '../src/reserve_contract'
+import Deployer from '../src/deployer'
+import { resolve } from 'url';
+
+const provider = ganache.provider()
+const web3 = new Web3(provider)
+const kyberNetworkAddress = '0x91a502C678605fbCe581eae053319747482276b9'
+
+let addresses
+beforeEach(async () => {
+  const dpl = new Deployer(provider)
+  addresses = await dpl.deploy(
+    { address: (await dpl.web3.eth.getAccounts())[0] },
+    kyberNetworkAddress,
+    false
+  )
+})
+
+describe('ReserveContract', () => {
+  it('failed to create an instance if provider is not provided', () => {
+    assert.throws(() => {
+      ReserveContract(undefined, addresses.reserve)
+    })
+  })
+
+  it('failed to create an instance if address is invalid', () => {
+    assert.throws(() => {
+      ReserveContract(provider, '')
+    })
+    assert.throws(() => {
+      ReserveContract(provider, 'invalid-address')
+    })
+  })
+
+  it('created an reserver contract instance successfully', () => {
+    const reserveContract = new ReserveContract(provider, addresses.reserve)
+    assert.ok(reserveContract.contract)
+  })
+
+  it('is able to alter trade status', async () => {
+    const reserveContract = new ReserveContract(provider, addresses.reserve)
+    const accounts = await web3.eth.getAccounts()
+    const account = {address: accounts[0]}
+    await assert.ok(await reserveContract.enableTrade(account))
+    await assert.strictEqual(await reserveContract.tradeEnabled(), true)
+    // it cannot disableTrade from a non alerter account 
+    await assertThrowAsync( async () => await reserveContract.disableTrade(account))
+    // it can disableTrade from an alerter account
+    await reserveContract.addAlerter(account, accounts[1])
+    await assert.ok(await reserveContract.disableTrade({address: accounts[1]}))
+    await assert.strictEqual(await reserveContract.tradeEnabled(), false)
+  })
+
+  it('is able to setContracts addresses', async () => {
+    const reserveContract = new ReserveContract(provider, addresses.reserve)
+    const accounts = await web3.eth.getAccounts()
+    //it should run Ok with valid addresses
+    await assert.ok(await reserveContract.setContracts({address: accounts[0]}, accounts[0], accounts[1], accounts[2]))
+    await assert.strictEqual(await reserveContract.kyberNetwork(), accounts[0])
+    await assert.strictEqual(await reserveContract.conversionRatesContract(), accounts[1])
+    await assert.strictEqual(await reserveContract.sanityRatesContract(), accounts[2])
+    // it should throw if kybernetwork and conversionRate is not valid
+    await assertThrowAsync( async() => await setContracts({address: accounts[0]}, "random", "random", accounts[2]))
+    // it should run ok without sanity Rates
+    await assert.ok(await reserveContract.setContracts({address: accounts[0]}, accounts[0], accounts[1], undefined))
+
+  })
+})
+
+async function assertThrowAsync(fn) {
+    let dummy = () => {}
+    try{ 
+        await(fn())
+    }catch(err) {
+        dummy = () => {throw err}
+    } finally {
+        assert.throws(dummy)
+    }
+}
