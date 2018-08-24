0331d12fd8bfb6b8f0ba18a9b3c0ac6406f45dca
kyberliem <cyberliem.civil@gmail.com>
2018-08-24 15:49:40 +0700

    implemented withdrawal approvals methods

--- src/reserve_contract.js
+++ src/reserve_contract.js
@@ -19,33 +19,50 @@ export default class ReserveContract extends BaseContract {
    */
   constructor (provider, address) {
     super(provider, address)
-    const web3 = new Web3(provider)
-    this.contract = new web3.eth.Contract(reserveContractABI, address)
+    this.web3 = new Web3(provider)
+    this.contract = new this.web3.eth.Contract(reserveContractABI, address)
   }
 
-  enableTrade (account) {
-    return this.contract.methods.enableTrade().send({ from: account.address })
+  async enableTrade (account) {
+    const med = this.contract.methods.enableTrade()
+    return this.contract.methods.enableTrade().send({
+      from: account.address,
+      gas: await med.estimateGas({
+        from: account.address
+      })
+    })
   }
+
   tradeEnabled () {
     return this.contract.methods.tradeEnabled().call()
   }
-  disableTrade (account) {
-    return this.contract.methods.disableTrade().send({ from: account.address })
+  async disableTrade (account) {
+    const med = this.contract.methods.disableTrade()
+    return med.send({
+      from: account.address,
+      gas: await med.estimateGas({
+        from: account.address
+      })
+    })
   }
   /**
    * set Contract addresses for reserver contract.
    * @param {object} account - admin account.
    * @param {string} network - address of kyber network smart contract.
    * @param {string} conversion - address of kyber network smart contract.
-   * @param {string} sanitys (optional) - address of sanity rates contract.
+   * @param {string} sanity (optional) - address of sanity rates contract.
    * @returns {txObject} - the tx object of send() command from this contract method
    */
-  setContracts (account, network, conversion, sanity) {
+  async setContracts (account, network, conversion, sanity) {
     validateAddress(network)
     validateAddress(conversion)
-    return this.contract.methods
-      .setContracts(network, conversion, sanity)
-      .send({ from: account.address })
+    const med = this.contract.methods.setContracts(network, conversion, sanity)
+    return med.send({
+      from: account.address,
+      gas: await med.estimateGas({
+        from: account.address
+      })
+    })
   }
 
   conversionRatesContract () {
@@ -58,5 +75,60 @@ export default class ReserveContract extends BaseContract {
 
   kyberNetwork () {
     return this.contract.methods.kyberNetwork().call()
+  }
+
+  /**
+   * approve/disapprove withdraw address for a token on reserve.
+   * @param {object} account - admin account.
+   * @param {string} tokenAddress - contract address of the modifying token.
+   * @param {string} withdrawAddress - address for withdrawal.
+   * @param {boolean} approve - true if approving, false for disapproving.
+   * @returns {txObject} - the tx object of send() command from this contract method
+   */
+  async approveWithdrawAddress (
+    account,
+    tokenAddress,
+    withdrawAddress,
+    approve
+  ) {
+    const med = this.contract.methods.approveWithdrawAddress(
+      tokenAddress,
+      withdrawAddress,
+      approve
+    )
+    return med.send({
+      from: account.address,
+      gas: await med.estimateGas({
+        from: account.address
+      })
+    })
+  }
+
+  /**
+   * withdraw ether
+   * @param {object} account - admin account.
+   * @param {string} amount - amount to withdraw, must be in wei.
+   * @param {string} address - address for withdrawal.
+   * @returns {txObject} - the tx object of send() command from this contract method
+   */
+  async withdrawEther (account, amount, address) {
+    const med = this.contract.methods.withdrawEther(amount, address)
+    return med.send({
+      from: account.address,
+      gas: await med.estimateGas({
+        from: account.address
+      })
+    })
+  }
+
+  /**
+   * check for approval status of a token address to a particular address
+   * @param {object} address - address to withdraw the token to
+   * @param {string} tokenAddress - address of the token's smart contract. Must be deployed already.
+   * @returns {bool} - true for approved, false otherwise
+   */
+  approvedWithdrawAddresses (address, tokenAddress) {
+    const addressHash = Web3.utils.soliditySha3(tokenAddress, address)
+    return this.contract.methods.approvedWithdrawAddresses(addressHash).call()
   }
 }
--- test/reserve_contract.test.js
+++ test/reserve_contract.test.js
@@ -4,7 +4,7 @@ import ganache from 'ganache-cli'
 
 import ReserveContract from '../src/reserve_contract'
 import Deployer from '../src/deployer'
-
+import ERC20TokenDeployer from './deploy_erc20'
 const provider = ganache.provider()
 const web3 = new Web3(provider)
 const kyberNetworkAddress = '0x91a502C678605fbCe581eae053319747482276b9'
@@ -42,7 +42,7 @@ describe('ReserveContract', () => {
 
   it('is able to alter trade status', async () => {
     const reserveContract = new ReserveContract(provider, addresses.reserve)
-    const accounts = await web3.eth.getAccounts()
+    const accounts = await reserveContract.web3.eth.getAccounts()
     const account = { address: accounts[0] }
     await assert.ok(await reserveContract.enableTrade(account))
     await assert.strictEqual(await reserveContract.tradeEnabled(), true)
@@ -99,6 +99,50 @@ describe('ReserveContract', () => {
       )
     )
   })
+
+  it('is able to change withdrawal approvement status', async () => {
+    console.log('testing approval status...')
+    const reserveContract = new ReserveContract(provider, addresses.reserve)
+    const accounts = await web3.eth.getAccounts()
+    const tokenAddr = await ERC20TokenDeployer(provider)
+    await assert.ok(
+      await reserveContract.approveWithdrawAddress(
+        { address: accounts[0] },
+        tokenAddr,
+        accounts[1],
+        true
+      )
+    )
+    await assert.strictEqual(
+      await reserveContract.approvedWithdrawAddresses(accounts[1], tokenAddr),
+      true
+    )
+    await assert.ok(
+      await reserveContract.approveWithdrawAddress(
+        { address: accounts[0] },
+        tokenAddr,
+        accounts[1],
+        false
+      )
+    )
+    await assert.strictEqual(
+      await reserveContract.approvedWithdrawAddresses(accounts[1], tokenAddr),
+      false
+    )
+  })
+
+  //    console.log(await web3.eth.getBalance(accounts[0]))
+  //    console.log(await web3.eth.getBalance(accounts[1]))
+
+  //    const restx =await web3.eth.sendTransaction({
+  //        from : accounts[0],
+  //        to : addresses.reserve,
+  //        value : "1"
+  //    })
+  //    console.log(restx)
+  //    console.log(await web3.eth.getBalance(accounts[0]))
+  //    await reserveContract.withdrawEther({address: accounts[0]}, "1", accounts[1])
+  //    console.log(await web3.eth.getBalance(accounts[1]))
 })
 
 async function assertThrowAsync (fn) {
