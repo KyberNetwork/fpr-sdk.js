1501f57d0a23a95560ce4cd6c16efe16b7445b94
kyberliem <cyberliem.civil@gmail.com>
2018-09-12 13:49:36 +0700

    throw err instead in address assert

--- src/permission_assert.js
+++ src/permission_assert.js
@@ -2,23 +2,25 @@ export const assertOperator = async (contract, address) => {
   const operators = await contract.getOperators()
   for (var op in operators) {
     if (op.toLowerCase() === address.toLowerCase()) {
-      return true
+      return
     }
   }
-  return false
+  throw new Error(`not operator address: '${address}'`)
 }
 
 export const assertAlerter = async (contract, address) => {
   const alerters = await contract.getAlerters()
   for (var alerter in alerters) {
     if (alerter.toLowerCase() === address.toLowerCase()) {
-      return true
+      return
     }
   }
-  return false
+  throw new Error(`not alerter address: '${address}'`)
 }
 
 export const assertAdmin = async (contract, address) => {
-  const admin = contract.admin()
-  return admin.toLowerCase() === address.toLowerCase()
+  const admin = await contract.admin()
+  if (admin.toLowerCase() !== address.toLowerCase()) {
+    throw new Error(`not alerter address address: '${address}'`)
+  }
 }
