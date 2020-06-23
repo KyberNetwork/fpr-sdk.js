6bc08f4143d11666edc6ccdf9a2a0eaab68016fd
meow <mingyeow@gmail.com>
2020-06-23 15:08:06 +0800

    minor changes

--- script/deploy.js
+++ script/deploy.js
@@ -1,11 +1,11 @@
 import Web3 from "web3"
-import Deployer from "./src/deployer.js"
+import Deployer from "../src/deployer.js"
 
-const provider = new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/27453d291e004037ac4734bd0138dc0f')
+const provider = new Web3.providers.HttpProvider(process.env.WEB3_NODE_ADDRESS)
 const dpl = new Deployer(provider)
 
 // initialize account from private key
-const account = dpl.web3.eth.accounts.privateKeyToAccount('2C9CFCEE9048E0D6B481E58F6E874368682B5FD01DD2BE267E71C4329ED3AFF5')
+const account = dpl.web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY)
 
 dpl.web3.eth.accounts.wallet.add(account)
 
@@ -14,4 +14,5 @@ let addresses;
     addresses = await dpl.deploy(account)
     console.log("Reserve deployed at:")
     console.log(addresses)
+    console.log("TODO: give the etherscan link here")
 })()
