88c13aaaafdb5dc642a6b7bb5b97ee4e758ba786
meow <mingyeow@gmail.com>
2020-06-23 15:42:49 +0800

    change name

--- bin/testnet_deploy.js
+++ bin/testnet_deploy.js
@@ -3,10 +3,9 @@
 import Web3 from "web3"
 import Deployer from "../src/deployer.js"
 
-const provider = new Web3.providers.HttpProvider(process.env.TESTNET_NODE_URL)
+const provider = new Web3.providers.HttpProvider(process.env.ROPSTEN_NODE_URL)
 const dpl = new Deployer(provider)
 
-// initialize account from private key
 const account = dpl.web3.eth.accounts.privateKeyToAccount(process.env.TESTNET_PRIVATE_KEY)
 
 dpl.web3.eth.accounts.wallet.add(account)
@@ -14,7 +13,6 @@ dpl.web3.eth.accounts.wallet.add(account)
 let addresses;
 (async () => {
     addresses = await dpl.deploy(account)
-    console.log("Reserve deployed at:")
+    console.log("Reserves deployed at:")
     console.log(addresses)
-    console.log("TODO: give the etherscan link here")
 })()
