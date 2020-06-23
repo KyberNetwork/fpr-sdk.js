167027c05f5ddc8f27cd531e0540aa71ebc81d77
meow <mingyeow@gmail.com>
2020-06-23 17:02:20 +0800

    update packaages

--- src/conversion_rates_contract.js
+++ src/conversion_rates_contract.js
@@ -1,5 +1,5 @@
 import Web3 from 'web3'
-import { BigNumber } from 'bignumber.js'
+import BigNumber from 'bignumber.js'
 
 import BaseContract from './base_contract.js'
 import conversionRatesABI from '../contracts/ConversionRatesContract.abi.json'
